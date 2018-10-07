f5fbb2151cac18237d44b5a00342b6fc9e29442a
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2018-10-07 07:43:51 -0600

    Add h1 to main

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ var pageBody = document.getElementsByTagName('body')[0];
 var scriptTag = document.getElementsByTagName('script')[0];
 pageBody.insertBefore(main, scriptTag);
 
-console.log(pageBody);
+var header = document.createElement('h1');
+main.appendChild(header);
+
+console.log(header);

a5946253746faf8c0c7ee8536ade485005d43b70
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2018-10-07 07:34:37 -0600

    Add main, body, and script tagss to the page; placed them in order

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 var main = document.createElement('main');
-console.log('main');
+var pageBody = document.getElementsByTagName('body')[0];
+var scriptTag = document.getElementsByTagName('script')[0];
+pageBody.insertBefore(main, scriptTag);
+
+console.log(pageBody);
